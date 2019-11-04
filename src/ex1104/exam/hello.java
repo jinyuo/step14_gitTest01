67bb6dc09a06ab12f9e8a55bdbad2568f6005114
jinyuo <aleydis95@gmail.com>
2019-11-04 17:20:14 +0900

    second modify : hello.java, +TestService.java

--- src/ex1104/exam/hello.java
+++ src/ex1104/exam/hello.java
@@ -5,6 +5,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
+		
+		System.out.println("아, 내 다음 아이디……. 없어졌어…… 억울하다.");
 	}
 
 }
