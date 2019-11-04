7d9e8a3b85ee0b9628069068dd0581cc17dd2bbb
jinyuo <aleydis95@gmail.com>
2019-11-04 17:23:56 +0900

    third modify : TestService.java

---
+++ src/ex1104/exam/service/TestService.java
@@ -0,0 +1,13 @@
+package ex1104.exam.service;
+
+public class TestService {
+	public void backMyAccount() {
+		System.out.println("please give me back my account.");
+		System.out.println("kakao¡¦¡¦.");
+	}
+	
+	public void why() {
+		int i = 6;
+		System.out.println(i);
+	}
+}
